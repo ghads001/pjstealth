bf21f04efcdd81ab5251177febf63f792443cf3e
ghads001 <undoing-duh-neatly@duck.com>
2025-01-01 16:05:05 +0800

    Update camoufoxtest.js

--- camoufoxtest.js
+++ camoufoxtest.js
@@ -1,7 +1,19 @@
 const { chromium } = require('patchright');
+const { newInjectedContext } require('fingerprint-injector');
 
 (async () => {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({ headless: false });
+  const context = await newInjectedContext(
+      browser,
+      {
+            // Constraints for the generated fingerprint (optional)
+          fingerprintOptions: {
+              devices: ['mobile'],
+              operatingSystems: ['ios'],
+          },
+          
+        },
+    );
   const page = await browser.newPage();
   await page.goto('https://httpbin.co/anything');
   console.log(await page.content());
