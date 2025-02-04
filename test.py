0464bd7f2a724afd65d6f87ac661ebbd621d0b70
ghads001 <undoing-duh-neatly@duck.com>
2025-02-04 16:08:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from DrissionPage import Chromium, ChromiumOptions
-co = ChromiumOptions().set_browser_path(r'./chrome/ungoogled-chromium_130.0.6723.116-1_linux/chrome').set_local_port(9111).set_user_data_path(r'./test1')
-co.set_argument('--fingerprint','2')
+co = ChromiumOptions().set_browser_path(r'./chrome/ungoogled-chromium_130.0.6723.116-1_linux/chrome').set_local_port(9111).set_user_data_path(r'./test2')
+co.set_argument('--fingerprint','1002')
+co.set_argument('--fingerprint-platform','macos')
 co.set_argument('--fingerprinting-canvas-image-data-noise')
 co.set_argument('--fingerprinting-canvas-measuretext-noise')
 co.set_argument('--fingerprinting-client-rects-noise')
-co.set_argument('--lang','de')
-co.set_argument('--accept-lang','de')
+co.set_argument('--lang','zh')
+co.set_argument('--accept-lang','zh')
 co.set_argument('--no-sandbox')
 browser = Chromium(co)
 tab = browser.latest_tab
