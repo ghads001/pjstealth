d455871636393f5b3e3de5f417833ff01bcc99a6
ghads001 <undoing-duh-neatly@duck.com>
2025-02-04 15:39:00 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from DrissionPage import Chromium, ChromiumOptions
 co = ChromiumOptions().set_browser_path(r'./chrome/ungoogled-chromium_131.0.6778.264-1_linux/chrome').set_local_port(9111).set_user_data_path(r'./test1')
-co.set_argument('--fingerprint', '1')
-co.set_argument('--fingerprint-platform', 'windows')
+co.set_argument('--fingerprint', '1000')
+co.set_argument('--fingerprint-platform', 'macos')
 co.set_argument('--fingerprint-brand', 'Vivaldi')
 co.set_argument('--fingerprint-hardware-concurrency', '16')
 co.set_argument('--fingerprinting-canvas-image-data-noise')
