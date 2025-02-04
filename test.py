8d81d05d0b32ceba0c7f59fd933975adfd246078
ghads001 <undoing-duh-neatly@duck.com>
2025-02-04 16:04:55 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from DrissionPage import Chromium, ChromiumOptions
-co = ChromiumOptions().set_browser_path(r'./chrome/ungoogled-chromium_131.0.6778.264-1_linux/chrome').set_local_port(9111).set_user_data_path(r'./test1')
+co = ChromiumOptions().set_browser_path(r'./chrome/ungoogled-chromium_130.0.6723.116-1_linux/chrome').set_local_port(9111).set_user_data_path(r'./test1')
 co.set_argument('--fingerprint','2')
 co.set_argument('--fingerprinting-canvas-image-data-noise')
 co.set_argument('--fingerprinting-canvas-measuretext-noise')
